f634e94bbb546526f8f21f11a700bcc5f5309b5f
viviangm10-cmd <vivianmaria10@gmail.com>
2026-09-14 13:25:12 -0300

    feat:ola mundo inicial

--- 01_nocoes_e_fundamentos/olamundo.py
+++ 01_nocoes_e_fundamentos/olamundo.py
@@ -1,2 +1,8 @@
 print("vivian")
 print("ola mundo")
+
+nome =input ("digite seu nome:")
+dia = input ("digite o dia do seu nascimento:")
+mes = input ("digite o mês do seu nascimento:")
+ano = input ("digite o ano do seu nascimento:")
+print("Olá", nome, "você nasceu no dia", dia, "do mês", mes, "do ano de", ano)
