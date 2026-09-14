1c14ae01ef36b8a01251a7ed83658341213c9b20
viviangm10-cmd <vivianmaria10@gmail.com>
2026-09-14 14:42:17 -0300

    primeiro codigo teste

---
+++ 02_conceitos_basicos/pratica_aluno_mao_na_massa.py
@@ -0,0 +1,12 @@
+# TODO: Declare as variáveis abaixo e execute a célula para testar
+nome_produto = "vivian shampoos sushi e banana"
+quantidade_estoque = 1000.0
+preco_unitario = 50.0
+disponivel_para_venda = True
+
+# TODO: Escreva os prints de inspeção aqui
+print("Nome do produto:", nome_produto)
+print("quantidade em estoque:", quantidade_estoque)
+print("preço unitario:", preco_unitario)
+print("disponivel para venda;", disponivel_para_venda)
+
---
+++ 02_conceitos_basicos/pratica_aluno_mao_na_massa_02.py
@@ -0,0 +1,12 @@
+# TODO: Desenvolva seu algoritmo aqui
+# 1. Leia o valor da conta (float)
+# 2. Leia o número de pessoas (int)
+# 3. Calcule o valor por pessoa
+# 4. Imprima formatado usando f-string
+
+valor_da_conta = input("digite o valor da conta:")
+valor_da_conta = float(valor_da_conta)
+numero_de_pessoasna_mesa = input("digite o numero de pessoas na messa:")
+numero_de_pessoasna_mesa = int(numero_de_pessoasna_mesa)
+valor_da_conta_por_pessoa = valor_da_conta / numero_de_pessoasna_mesa
+
